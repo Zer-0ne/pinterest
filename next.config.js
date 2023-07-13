a5dad5a2acb44ab9c69962dd64731576d25d399d
Zer-0ne <sahilkhan8294799@gmail.com>
2023-07-13 09:17:59 +0530

    increase the payload limit of post media(image video)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com','firebasestorage.googleapis.com','firebasestorage.googleapis.com'],
+        domains: ['images.unsplash.com', 'firebasestorage.googleapis.com', 'firebasestorage.googleapis.com'],
     },
-    
+    serverRuntimeConfig: {
+        // Increase the payload size limit (in bytes)
+        maxPayloadSize: 100048576, // For example, set it to 100MB
+    },
 }
 
 module.exports = nextConfig
