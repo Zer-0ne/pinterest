73aa325f33824cba1a635546cbd6acdb54174f63
Zer-0ne <sahilkhan8294799@gmail.com>
2023-07-13 23:57:30 +0530

    increase the size limit

--- next.config.js
+++ next.config.js
@@ -11,7 +11,9 @@ const nextConfig = {
     serverRuntimeConfig: {
         // Set the maximum payload size limit for server-side requests (in bytes)
         // Adjust the value as per your requirements
-        payloadLimit: '100mb'
+        payloadLimit: '100mb',
+        maxPayloadSize: '200mb',
+        maxPayload: 200 * 1024 * 1024
     },
     publicRuntimeConfig: {
         // Set the maximum payload size limit for client-side requests (in bytes)
