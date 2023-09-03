5a3765968cc23c0e0978f0752214b57eef254db5
Sahil Khan <118300201+Zer-0ne@users.noreply.github.com>
2023-09-03 13:25:58 +0000

    increase the server api limit

--- next.config.js
+++ next.config.js
@@ -13,17 +13,21 @@ const nextConfig = {
         // Adjust the value as per your requirements
         payloadLimit: '100mb',
         maxPayloadSize: '200mb',
-        maxPayload: 200 * 1024 * 1024
+        maxPayload: 200 * 1024 * 1024,
+
     },
     publicRuntimeConfig: {
         // Set the maximum payload size limit for client-side requests (in bytes)
         // Adjust the value as per your requirements
         payloadLimit: '100mb'
     },
-    experimental:{
-        serverActions:true,
-        serverActionsBodySizeLimit:'100mb'
-    }
+    experimental: {
+        serverActions: true,
+        serverActionsBodySizeLimit: '100mb'
+    },
+    api: {
+        responseLimit: false,
+    },
 }
 
 module.exports = nextConfig
